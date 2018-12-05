f3f90eae66459a183b2d11dee91e85243d96de5e
Libyan Developer <libyandeveloper@Libyans-MacBook-Pro.local>
2018-12-05 09:06:53 +0200

    first commit

---
+++ changeLanguage.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="Change language of portal">
+    <meta name="author" content="Hayfa">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Change Language</title>
+
+    <link rel="stylesheet" type="text/css" href="asset/css/bootstrap.min.css">
+    <link href="asset/css/style.css" rel="stylesheet">
+
+</head>
+<body>  
+        <style type="text/css">
+            .goog-logo-link {
+           display:none !important;
+        }
+        .goog-te-gadget{
+           color: transparent !important;
+        }
+        .goog-te-gadget .goog-te-combo{
+           color: black !important;
+        }
+        </style>
+        <nav class="navbar navbar-default header navbar-fixed-top" style="background-color:cadetblue;">
+            <div class="col-md-12 nav-wrapper">
+
+                <div class="navbar-header" style="width:100%;">
+
+                <div class="nav navbar-nav navbar-right user-nav" id="google_translate_element" style="margin-top: 15px;"></div>
+                <script type="text/javascript">
+                function googleTranslateElementInit() {
+                    new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+                }
+                </script>
+                <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
+                </div>
+              </div>
+            </div>
+        </nav>
+
+        <div align="center" style="margin-top:200px;">
+            <h1  style="color:cadetblue;">Welcome to our Website.</h1>
+        </div>
+
+        <script src="asset/js/bootstrap.min.js"></script>
+        <script src="asset/js/main.js"></script>
+            
+</body>
+</html>
